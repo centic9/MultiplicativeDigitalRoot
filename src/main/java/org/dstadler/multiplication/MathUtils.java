2e187229d068b0140d30b9c645be445ae657f5df
Dominik Stadler <dominik.stadler@gmx.at>
2019-09-29 20:39:51 +0200

    Add JavaDoc

--- src/main/java/org/dstadler/multiplication/MathUtils.java
+++ src/main/java/org/dstadler/multiplication/MathUtils.java
@@ -7,8 +7,11 @@ import java.util.List;
 /**
  * Utilities for computing multiplicative persistence and multiplicative digital root
  *
+ * See https://en.wikipedia.org/wiki/Multiplicative_digital_root and
+ * https://en.wikipedia.org/wiki/Persistence_of_a_number
  */
 public class MathUtils {
+    // Pre-compute digits from 0-9 to not having to construct them always
     private static final BigInteger[] DIGITS = new BigInteger[10];
     static {
         for(int i = 0;i < 10;i++) {
@@ -16,7 +19,18 @@ public class MathUtils {
         }
     }
 
-
+    /**
+     * Compute the multiplicative persistence of a number, see
+     * also https://en.wikipedia.org/wiki/Persistence_of_a_number and
+     *
+     * E.g. 10 => 1, 25 => 2, 39 => 3, ...
+     *
+     * See also https://oeis.org/A007954 and https://oeis.org/A003001 for related
+     * mathematical series.
+     *
+     * @param input The number as string
+     * @return How many times the multiplicative digital root can be computed on this number.
+     */
     public static int getPersistence(String input) {
         if(input.contains("0")) {
             return 1;
@@ -37,6 +51,19 @@ public class MathUtils {
         }
     }
 
+    /**
+     * Compute the multiplicative digital root of the given number.
+     *
+     * See also https://en.wikipedia.org/wiki/Multiplicative_digital_root
+     *
+     * E.g. 10 => 0, 25 => 10, 39 => 27, 27 => 14, 14 => 4, ...
+     *
+     * See also https://oeis.org/A007954 and https://oeis.org/A003001 for related
+     * mathematical series.
+     *
+     * @param input The number as string
+     * @return The multiplicative root of the given number.
+     */
     public static BigInteger getMultiplicativeDigitalRoot(String input) {
         // shortcut this calculation
         if(input.contains("0")) {
@@ -45,11 +72,23 @@ public class MathUtils {
 
         BigInteger product = BigInteger.ONE;
         for (char digit: input.toCharArray()) {
-            product = product.multiply(DIGITS[digit - 0x30]);
+            // multiplying by "1" does not change the result
+            if(digit != '1') {
+                product = product.multiply(DIGITS[digit - 0x30]);
+            }
         }
+
         return product;
     }
 
+    /**
+     * Simple algorithm for computing prime factors for BigIntegers.
+     * Mostly used in testing/evaluation of known smallest numbers for
+     * multiplicative persistence.
+     *
+     * @param input The number to factor
+     * @return A sorted list of all factors of the given number.
+     */
     public static List<BigInteger> primeFactors(String input) {
         /*        int n = numbers;
         List<Integer> factors = new ArrayList<Integer>();
@@ -93,6 +132,21 @@ public class MathUtils {
         return factors;
     }
 
+    /**
+     * Compute the base-10 logarithm of the given BigInteger.
+     * <br/><br/>
+     * log10(0) => 0<br/>
+     * log10(1) => 0<br/>
+     * ...<br/>
+     * log10(9) => 0<br/>
+     * log10(10) => 1<br/>
+     * ...<br/>
+     * log10(100) => 2<br/>
+     * ...<br/>
+     *
+     * @param input the number used for computation
+     * @return the logarithm of the given number in base 10
+     */
     public static int log10(BigInteger input) {
         int digits = 0;
         int bits = input.bitLength();
