a7f0624baef59b0c19d2c43ea4c62b2aceb04ee1
Dominik Stadler <dominik.stadler@gmx.at>
2019-09-29 10:54:02 +0200

    Play with primeFactors and start verifying numbers in ascending order

--- src/main/java/org/dstadler/multiplication/MultiplicativeDigitalRoot.java
+++ src/main/java/org/dstadler/multiplication/MultiplicativeDigitalRoot.java
@@ -1,13 +1,19 @@
 package org.dstadler.multiplication;
 
-import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MultiplicativeDigitalRoot {
     public static void main(String[] args) {
 
         int maxPersistence = 1;
+        int count = 0;
+
+        BigInteger number = new BigInteger("0");
+        long start = System.currentTimeMillis();
 
         while(true) {
             /*System.out.println("Please enter a number: ");
@@ -16,14 +22,23 @@ public class MultiplicativeDigitalRoot {
                 break;
             }*/
 
-            String input = RandomStringUtils.random(100, "12345789");
+            //String input = RandomStringUtils.random(RandomUtils.nextInt(16, 100), "12345789");
+            number = number.add(BigInteger.ONE);
+            String input = number.toString();
 
-            System.out.println("Calculating the persistence of " + input);
+            //System.out.println("Calculating the persistence of " + input);
             int persistence = getPersistence(input);
 
-            System.out.println("Had persistence: " + persistence + " for " + input);
+            if(persistence >= 10 || count % 100000 == 0) {
+                long now = System.currentTimeMillis();
+                long duration = (now - start)/1000;
+                System.out.println(String.format("%15d: Had persistence: %2d for %s, max: %2d, n/sec: %,15d",
+                        count, persistence, StringUtils.leftPad(input, 15, " "), maxPersistence,
+                        (duration == 0 ? BigInteger.ZERO : number.divide(BigInteger.valueOf(duration)))));
+            }
 
             maxPersistence = Math.max(persistence, maxPersistence);
+            count++;
 
             if(persistence > 11) {
                 break;
@@ -55,5 +70,48 @@ public class MultiplicativeDigitalRoot {
             product = product.multiply(new BigInteger("" + input.charAt(i)));
         }
         return product;
+    }
+
+    public static List<BigInteger> primeFactors(String input) {
+        /*        int n = numbers;
+        List<Integer> factors = new ArrayList<Integer>();
+        for (int i = 2; i <= n / i; i++) {
+            while (n % i == 0) {
+                factors.add(i);
+                n /= i;
+            }
+        }
+        if (n > 1) {
+            factors.add(n);
+        }
+        return factors;*/
+
+        BigInteger n = new BigInteger(input);
+        List<BigInteger> factors = new ArrayList<>();
+        BigInteger i = BigInteger.TWO;
+        while(true) {
+            BigInteger division = n.divide(i);
+            if(i.compareTo(division) > 0) {
+                break;
+            }
+
+            while(true) {
+                BigInteger remainder = n.remainder(i);
+                if(remainder.equals(BigInteger.ZERO)) {
+                    factors.add(i);
+                    n = n.divide(i);
+                } else {
+                    break;
+                }
+            }
+
+            i = i.add(BigInteger.ONE);
+        }
+
+        if (n.compareTo(BigInteger.ONE) > 0) {
+            factors.add(n);
+        }
+
+        return factors;
     }
 }
--- src/test/java/org/dstadler/multiplication/MultiplicativeDigitalRootTest.java
+++ src/test/java/org/dstadler/multiplication/MultiplicativeDigitalRootTest.java
@@ -2,6 +2,10 @@ package org.dstadler.multiplication;
 
 import org.junit.Test;
 
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class MultiplicativeDigitalRootTest {
@@ -30,6 +34,10 @@ public class MultiplicativeDigitalRootTest {
     public void testPersistence() {
         checkPersistence(1, "1");
         checkPersistence(1, "2");
+
+        // https://oeis.org/A003001
+        // 0, 10, 25, 39, 77, 679, 6788, 68889, 2677889, 26888999, 3778888999, 277777788888899
+        //checkPersistence(0, "0");
         checkPersistence(1, "10");
         checkPersistence(2, "25");
         checkPersistence(3, "39");
@@ -41,9 +49,60 @@ public class MultiplicativeDigitalRootTest {
         checkPersistence(9, "26888999");
         checkPersistence(10, "3778888999");
         checkPersistence(11, "277777788888899");
+
+        checkPersistence(11, "1277777788888899");
+        checkPersistence(11, "12777777888888991");
+        checkPersistence(11, "2777777888888991");
+        checkPersistence(11, "27777778888889911");
+
+        checkPersistence(11, "84772818479372227374");
     }
 
     private void checkPersistence(int expected, String input) {
         assertEquals(expected, MultiplicativeDigitalRoot.getPersistence(input));
+    }
+
+    @Test
+    public void testPrimeFactorization() {
+        checkFactorization("0");
+        checkFactorization("1");
+        checkFactorization("2", "2");
+        checkFactorization("3", "3");
+        checkFactorization("4", "2", "2");
+        checkFactorization("10", "2", "5");
+        checkFactorization("13", "13");
+        checkFactorization("24", "2", "2", "2", "3");
+
+        // https://oeis.org/A003001
+        // 0, 10, 25, 39, 77, 679, 6788, 68889, 2677889, 26888999, 3778888999, 277777788888899
+        checkFactorization("0");
+        checkFactorization("10", "2", "5");
+        checkFactorization("25", "5", "5");
+        checkFactorization("39", "3", "13");
+        checkFactorization("77", "7", "11");
+        checkFactorization("679", "7", "97");
+        checkFactorization("6788", "2", "2", "1697");
+        checkFactorization("68889", "3", "22963");
+        checkFactorization("2677889", "29", "107", "863");
+        checkFactorization("26888999", "523", "51413");
+        checkFactorization("3778888999", "3778888999");
+        checkFactorization("277777788888899", "13", "59", "1699", "213161503");
+
+        checkFactorization("1277777788888899", "3", "425925929629633");
+        checkFactorization("2777777888888991", "3", "113", "3011", "2721366679");
+        checkFactorization("27777778888889911", "7", "7", "23", "619", "8447", "4713901");
+        checkFactorization("84772818479372227374", "2", "3", "31", "10891", "137191", "305035439");
+    }
+
+    private void checkFactorization(String input, String... expectedFactors) {
+        List<BigInteger> factors = MultiplicativeDigitalRoot.primeFactors(input);
+
+        assertEquals("Did not get expected factors, expected " + Arrays.toString(expectedFactors) + " but had " + factors,
+                expectedFactors.length, factors.size());
+
+        for(int i = 0;i < factors.size();i++) {
+            assertEquals("Did not get expected factor at " + i + " for " + Arrays.toString(expectedFactors) + ", had: " + factors,
+                    expectedFactors[i], factors.get(i).toString());
+        }
     }
 }
